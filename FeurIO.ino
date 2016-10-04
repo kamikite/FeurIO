beae8d9bf2d1744b0caf2256a991c1f7dbd43817
Quang <quang.d.chau@gmail.com>
2016-10-04 23:54:41 +0200

    renaming some functions, refactoring, fix some delay bugs

--- FeurIO.ino
+++ FeurIO.ino
@@ -6,38 +6,32 @@ uint8_t LED_GREEN = 6; //Fotowiderstand, TEST, Helligkeit der LEDS
 uint8_t LED_RED = 5; //Thermometer, TEST, Geschwindigkeit des Ventilator
 uint8_t LED_YELLOW = 4; // IR Reciever
 uint8_t LED_STRIP = 8; //Wert zuweisen
+uint8_t LED_STRIP_ELEMENT = 4;
 
 //PIN Zuweisungen Input
 int SCHRANKE = A2; // 450 + keine Schranke , 450 - Schranke, AUSSCHALTEN
-int SENSOR_INPUT = A0; // desto höher der Wert desto dunkler
+int PHOTO_INPUT = A0; // desto höher der Wert desto dunkler
 int TEMP_INPUT = A1; // desto höher der Wert desto wärmer
 uint8_t IR_INPUT = 7; // Einschalten
 int OHMMETER = A3; // Messen des Widerstands, FARBE 
-/* 1k = 1000~1100 gelb
- * 1.7k = 1700 ~ 1800 orange
- * 5.7k = 5800 ~ 5900 rot
- * 1k || 1.7k = 629 = 630 ~ 680 dunkelrot
- * 1k || 5.7k = 850 ~ 900 lila
- * 1.7k || 5.7k = 1060 = 1200-1500 türkis
- * 1k || 1.7k || 5.7k = 567 = 550~600 blau
- * Farbreihenfolge = gelb, orange, rot , dunkelrot, lila, türkis, blau
- * Default: R2 = 0;
- */
 
 // VALUES
-uint8_t sensorWert = 0;
-uint8_t tempWert = 0;
+uint8_t photoValue = 0;
+uint8_t tempValue = 0;
 int raw = 0;
-int Vin = 5;
+uint8_t Vin = 5;
 float Vout= 0;
 float R1= 10000;
 float R2= 0;
 float buffer= 0;
 bool state = false; // false = off, true = on;
+bool debug = false;
 //VARS
 IRrecv My_Receiver(IR_INPUT);
 IRdecode My_Decoder;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, LED_STRIP, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_STRIP_ELEMENT, LED_STRIP, NEO_GRB + NEO_KHZ800);
+
+
 
 void setup() {
   pinMode(LED_GREEN, OUTPUT);
@@ -51,29 +45,17 @@ void setup() {
 
 void loop() {
   raw = analogRead(OHMMETER);
-  sensorWert = analogRead(SENSOR_INPUT);
-  tempWert = analogRead(TEMP_INPUT);
-  Serial.print("Photo: ");
-  Serial.println(sensorWert);
-  Serial.print("Temperatur: ");
-  Serial.println(tempWert);
-  Serial.print("Lichtschranke: ");
-  Serial.println(analogRead(SCHRANKE));
+  photoValue = analogRead(PHOTO_INPUT);
+  tempValue = analogRead(TEMP_INPUT);
+  // ----- LED Brightness Feature
+  //Photosensor
+  if (photoValue >= 255){
+    photoValue = 255;
+  }
+  strip.setBrightness(photoValue);
 
-  if (sensorWert >= 255){
-    sensorWert = 255;
-  }
-  if(R2 > 550 && R2 < 600){
-    fireLight(strip.Color(0, 0, 255), strip.Color(0,160,160), 150);
-  } else if (state){
-    fireLight(strip.Color(255, 0, 0), strip.Color(255,69,0), 150);  
-  }
-  if (analogRead(SCHRANKE)< 540){
-    LEDoff();
-    state = false;
-  }
-  strip.setBrightness(sensorWert);
-   //Check Ohm
+  // ----- Block Add Feature
+  //Getting unknown OHM
   if (raw){
     buffer = raw * Vin;
     Vout= (buffer)/1024.0;
@@ -86,8 +68,26 @@ void loop() {
   } else {
     R2 = 0;
   }
-   
-  //Check IR Signal
+  //assign COLOR STATE
+  if(R2 > 550 && R2 < 600){ //3 Bloecke BLAU
+    fireLight(strip.Color(0, 0, 255), strip.Color(0,160,160), 150);
+  } else if(R2 > 1000 && R2 < 1100){ //1 Block GELB 
+    //todo
+  } else if(R2 > 1700 && R2 < 1800){ //1 Block ORANGE
+    //todo
+  } else if(R2 > 5800 && R2 < 5900){ //1 Block ROT
+    //todo
+  } else if(R2 > 630 && R2 < 680){ //2 Bloecke DUNKELROT
+    //todo
+  } else if(R2 > 850 && R2 < 900){ //2 Bloecke LILA
+    //todo
+  } else if(R2 > 1200 && R2 < 1500){ //2 Bloecke TUERKIS
+    //todo
+  } else if (state){ //Default
+    fireLight(strip.Color(255, 0, 0), strip.Color(255,69,0), 150);  
+  }
+
+  // ----- SWITCH DEVICE -> ON
   if (My_Receiver.GetResults(&My_Decoder)) {
     My_Decoder.decode();
     //Serial.println(My_Decoder.value);
@@ -100,21 +100,31 @@ void loop() {
     My_Receiver.resume();     //Restart the receiver
   }
 
-  // Check Photosensor
-  if (sensorWert >= 150){
+  // ----- SWITCH DEVICE -> OFF
+  if (analogRead(SCHRANKE)< 540){
+    LEDoff();
+    state = false;
+  }
+  
+  // ----- Visual DEBUG Lights
+  // Photosensor OUTPUT
+  if (photoValue >= 150){
     digitalWrite(LED_GREEN, HIGH);
-  } else if (sensorWert < 150){
+  } else if (photoValue < 150){
     digitalWrite(LED_GREEN, LOW);
   }
-
-  // Check Heatsensor
-  if (tempWert >= 170){
+  
+  // Heatsensor OUTPUT
+  if (tempValue >= 170){
     digitalWrite(LED_RED, HIGH);
-  } else if (tempWert < 170){
+  } else if (tempValue < 170){
     digitalWrite(LED_RED, LOW);
   }
   
-  delay(1000);
+  if (debug){
+    debugMode();
+    delay (1000);
+  }  
 } // Loop END
 
 void LEDoff(){
@@ -124,13 +134,21 @@ void LEDoff(){
   strip.show();
 }
 
+void debugMode(){
+  Serial.print("Photo: ");
+  Serial.println(photoValue);
+  Serial.print("Temperatur: ");
+  Serial.println(tempValue);
+  Serial.print("Lichtschranke: ");
+  Serial.println(analogRead(SCHRANKE));
+}
+
 void fireLight(uint32_t c, uint32_t f, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
     for (int q=0; q < 2; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+2) {
         strip.setPixelColor(i+q, c);    //turn every third pixel on
       }
-      strip.setBrightness(analogRead(SENSOR_INPUT));
+      strip.setBrightness(analogRead(PHOTO_INPUT));
       strip.show();
       if (analogRead(SCHRANKE)< 540){
         LEDoff();
@@ -141,6 +159,5 @@ void fireLight(uint32_t c, uint32_t f, uint8_t wait) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+2) {
         strip.setPixelColor(i+q, f);        //turn every third pixel off
       }
-    }
-  }
+   }
 }
